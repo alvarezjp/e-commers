2e87fe4a29ad314bdc4e4a51a4619e0a97480e7f
Jesus <6.jesus.a@gmail.com>
2023-02-04 19:41:39 -0300

    agregar un alert para saber si la pagina de git lo toma

--- javascript/java_index/linkVerProducto.js
+++ javascript/java_index/linkVerProducto.js
@@ -6,7 +6,8 @@ let galery = document.querySelector(".gallery");
 //Agrego el evento de click y una ves que se realiza el evento leo que tarjeta contenga la clase que yo quiero y asi realizo la accion
 galery.addEventListener("click", (evento) => {
     if (evento.target.classList.contains("gallery__section__text__link")) {
-        obtain(evento);          
+        obtain(evento);   
+        alert("Se ejecuto el click");
         window.location.href = "http://127.0.0.1:5500/ver_producto.html";
     }
 });
