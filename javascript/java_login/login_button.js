222884025464533f0c7aa48701aa7c5697ab6fb2
Jesus <6.jesus.a@gmail.com>
2023-02-28 23:07:12 -0300

    se actualizo la pagina de productos, y se crea archivo txt

--- javascript/java_login/login_button.js
+++ javascript/java_login/login_button.js
@@ -5,28 +5,24 @@ import { regexEmail } from "./regexEmail.js";
 let email;
 let password;
 let inspectEmail;
-
-
 let divButton = document.querySelector(".login__form__button");
 
-divButton.addEventListener("click", (evento) => {    
+divButton.addEventListener("click", (evento) => {
     evento.preventDefault(); // ver a futuro como no utilizarlo
 
     email = document.getElementById("email").value;
     password = document.getElementById("password").value;
     inspectEmail = regexEmail(email);
 
-    if(inspectEmail){
-        if(checkData(email,password)){
+    if (inspectEmail) {
+        if (checkData(email, password)) {
             accessPag();
-        }else{
+        } else {
             alert("El correo o la contraseña son incorrectos");
         }
-    } else{
+    } else {
         alert("Error al escribir el email");
     }
-    
-    
 });
 
 divButton.addEventListener("mouseover", (evento2) => {
